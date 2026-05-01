e454035672f0448ae9149fef6b135b09bf19ebc8
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-05-01 20:35:07 +0530

    new

--- V7/index.js
+++ V7/index.js
@@ -18,8 +18,18 @@ app.get('/file/:filename', function(req,res){
         res.render("show",{filename:req.params.filename, filedata:data});
     });
 } )
+app.get('/edit/:filename', function(req,res){
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', function(err, data){
+        res.render("edit",{filename:req.params.filename});
+    });
+} )
 app.post('/create', function(req,res){
    fs.writeFile(`./files/${req.body.title.split(' ').join('')}`,req.body.details, function(err){
+        res.redirect('/');
+   });
+} )
+app.post('/edit', function(req,res){
+   fs.rename(`./files/${req.body.previousTitle.split(' ').join('')}`, `./files/${req.body.title.split(' ').join('')}`, function(err){
         res.redirect('/');
    });
 } )
