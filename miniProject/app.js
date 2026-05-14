720b09e79facc020cc7b98ea4d68d6e2259df88f
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-05-14 21:55:32 +0530

    new

--- miniProject/app.js
+++ miniProject/app.js
@@ -7,8 +7,7 @@ const jwt=require('jsonwebtoken');
 const userModel=require("./models/user");
 const postModel=require("./models/post");
 const post = require('./models/post');
-const crypto=require("crypto");
-const multer=require('multer');
+const upload=require('./config/multer')
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -16,27 +15,20 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(cookieParser());
 
-const storage=multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,'./public/images/uploads')
-    },
-    filename: function(req,file,cb){
-        crypto.randomBytes(12,function(err,bytes){
-            const fn=bytes.toString("hex")+path.extname(file.originalname);
-            cb(null,fn)
-        })
-    }
-})
-const upload=multer({storage: storage})
+
 app.get("/",(req,res)=>{
     res.render("index");
 
 })
-app.get("/test",(req,res)=>{
-    res.render("test");
+app.get("/profileupload",(req,res)=>{
+    res.render("profileupload");
+
 })
-app.post("/upload",upload.single("image"),(req,res)=>{
-   console.log(req.file);
+app.post("/upload",isLoggedIn,upload.single("image"),async (req,res)=>{
+   let user= await userModel.findOne({email: req.user.email});
+   user.profilepic=req.file.filename;
+   await user.save();
+   res.redirect("/profile")
 
 })
 app.get("/profile",isLoggedIn,async(req,res)=>{
